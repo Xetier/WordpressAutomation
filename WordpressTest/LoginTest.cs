46d485104e442c79759ce14770ca8ed310cca1bb
Pieter Gleixner <pieter.gleixner@cignium.com>
2019-01-31 15:24:26 -0500

    Initial commit

---
+++ WordpressTest/LoginTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordpressAutomation;
+
+namespace WordpressTest
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void Admin_User_Can_Login()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs("admin")
+                .WithPassword("Metroid061")
+                .Login();
+
+            Assert.IsTrue(DashboardPage.IsAt, "Failed to login");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Driver.Close();
+        }
+    }
+}
---
+++ WordpressTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WordpressTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordpressTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd02aa48-fe8d-41eb-9467-d6c160d1dcbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
