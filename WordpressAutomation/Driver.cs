d062a67d2ce16617c0426bbb8fd0b00eb687aad4
Pieter Gleixner <pieter.gleixner@cignium.com>
2019-02-04 12:02:21 -0500

    Refactor Driver

--- WordpressAutomation/Driver.cs
+++ WordpressAutomation/Driver.cs
@@ -1,23 +1,35 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Threading;
 
 namespace WordpressAutomation
 {
     public class Driver
     {
-        public static IWebDriver Instance { get;  set; }
+        public static IWebDriver Instance { get; set; }
+        public static string BaseAddress
+        {
+            get
+            {
+                return "http://localhost/wordpress/";
+            }
+        }
 
         public static void Initialize()
         {
             Instance = new ChromeDriver();
             Instance.Manage().Window.Maximize();
-            //Instance.Manage().Timeouts().ImplicitWait(TimeSpan.FromSeconds(5));
         }
 
         public static void Close()
         {
             //Instance.Close();
+        }
+
+        internal static void Wait(TimeSpan timeSpan)
+        {
+            Thread.Sleep((int)(timeSpan.TotalSeconds * 1000));
         }
     }
 }
--- WordpressAutomation/LoginPage.cs
+++ WordpressAutomation/LoginPage.cs
@@ -8,7 +8,7 @@ namespace WordpressAutomation
     {
         public static void GoTo()
         {
-            Driver.Instance.Navigate().GoToUrl("http://localhost/wordpress/wp-login.php");
+            Driver.Instance.Navigate().GoToUrl(Driver.BaseAddress + "wp-login.php");
             var wait = new WebDriverWait(Driver.Instance, TimeSpan.FromSeconds(10));
             wait.Until(d => d.SwitchTo().ActiveElement().GetAttribute("id") == "user_login");
         }
